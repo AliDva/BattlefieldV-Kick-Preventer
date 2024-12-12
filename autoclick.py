d3f0ce81f4aa8792bef889ec8777e7a31466dc3b
AliDva <90774146+AliDva@users.noreply.github.com>
2024-12-12 17:28:33 +0800

    Update autoclick.py
    现在bfv将不再弹出到前台并且你需要使用窗口化才能正常使用此脚本

--- autoclick.py
+++ autoclick.py
@@ -1,13 +1,24 @@
-from pywinauto import Application
+from pywinauto import Application 
 import time
 import random
 
 app = Application().connect(title="Battlefield™ V")
 window = app.window(title="Battlefield™ V")
 
+
 while True:
     
-    window.click(coords=(200, 200))
-    interval = random.uniform(10, 30)
-    print(f"The next click will take place after {interval:.2f} seconds")
+
+
+    
+    # 最小化窗口
+    window.minimize()
+
+    # 后台点击
+    window.click(button='left', pressed='', coords=(1250, 1253), double=False, absolute=False)
+    window.click(button='left', pressed='', coords=(222, 1339), double=False, absolute=False)
+
+    # 等待一段时间
+    interval = random.uniform(150, 160) #随机休眠150-160秒，如果你想要更高请自行更改
+    print(f"下一次点击将在 {interval:.2f} 秒后进行")
     time.sleep(interval)
